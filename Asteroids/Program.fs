87a7533161b2e221c758a9059f0588e17085d1ac
professorK <jpkessel@purdue.edu>
2024-12-28 20:42:06 -0500

    rock motion working
    seems to e handling the rock motion

--- Asteroids/Program.fs
+++ Asteroids/Program.fs
@@ -25,53 +25,50 @@ type RockRec = {
     pos: Vector2D<float>
     velocity: Vector2D<float>
     rotation: float
-    size: RockSize
     rotVelocity: float
+    size: RockSize
+    
 }
 
-let MAX_VELOCITY = 0.5
-let MAX_ROT_VELOCITY = 0.005
+let ROCK_PPS = 20.0
+let ROCK_ROT_PPS = 0.005
 let mutable asteroidsList =List<RockRec>.Empty
         
 let random = System.Random()
 
 let MakeRandomRock() =
     {
-        size=RockSize.large;
-        //pos=Vector2D(random.NextDouble() * 800.0, random.NextDouble() * 600.0)
-        pos = Vector2D(400.0,300.0)
-        rotation=random.NextDouble()*Math.PI*2.0
-        velocity =Vector2D((random.NextDouble() * 2.0 - 1.0)*MAX_VELOCITY,
-                           (random.NextDouble() * 2.0 - 1.0)*MAX_VELOCITY)
-        rotVelocity=(random.NextDouble()*2.0-1.0)*MAX_ROT_VELOCITY
+        size=RockSize.large
+        pos=Vector2D(random.NextDouble() * 800.0,
+                     random.NextDouble() * 600.0)
+        velocity=Vector2D(
+            random.NextDouble() * ROCK_PPS/2.0,
+            random.NextDouble() * ROCK_PPS/2.0)
+        rotation = random.NextDouble() * 2.0 * Math.PI
+        rotVelocity = random.NextDouble() * ROCK_ROT_PPS
     }
 let UpdateRockPosition ( elapsedMS: float, rock:RockRec) =
-    //let newPos = rock.pos + Vector2D(
-    //                                rock.velocity.X*(float elapsedMS),
-    //                                rock.velocity.Y*(float elapsedMS))
+    let newPos = rock.pos + Vector2D(
+                                   rock.velocity.X*(float elapsedMS)*ROCK_PPS/1000.0,
+                                   rock.velocity.Y*(float elapsedMS)*ROCK_PPS/1000.0)
                    
     let newRot = rock.rotation + (rock.rotVelocity*(float elapsedMS))
-    {rock with  rotation=newRot}
+    {rock with  rotation=newRot ; pos=newPos}
     
     
 let DrawRock (window:Window,images:Image list, rock:RockRec) =
     let rockImage = images.[(0)] //t)rock.size]
     let imageSize = rockImage.Size
     let matrix =
-       //Window.CreateTranslation(
-       //         Vector2(float32 rock.pos.X,float32 rock.pos.Y))*
-        Window.CreateTranslation(
-                Vector2(400f,300f))
-                //Vector2(400.0f-(float32 imageSize.Width/2.0f),
-                //        300.0f-(float32 imageSize.Height/2.0f))) *                                                                                  Window.CreateRotation(float32 rock.rotation)
-        //Window.CreateRotation(float32 rock.rotation)
+        Window.CreateRotation(float32 rock.rotation) *
+        Window.CreateTranslation(Vector2(float32 rock.pos.X, float32 rock.pos.Y))
     Window.DrawImage rockImage matrix |> ignore
     
 
 //execution starts here
 [<EntryPoint>]
 let main argv =
-    asteroidsList <- [0] |> List.map (fun _ -> MakeRandomRock())
+    asteroidsList <- [0..5] |> List.map (fun _ -> MakeRandomRock())
  
     // Loading the PLugins
     // We are doing it manually here, but in a real application
