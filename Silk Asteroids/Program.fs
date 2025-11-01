a2e15d43e8a3520790fe84be0e8731cf4805b505
professorK <jeffpk@gmail.com>
2025-11-01 13:26:44 -0400

    adding new launch shell for Silk managed Asteroids

---
+++ Silk Asteroids/Program.fs
@@ -0,0 +1,8 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open Asteroids.Main
+open ManagerRegistry
+open ConsoleLogger
+[<EntryPoint>]
+let start argv =
+    addManager typedefof<ConsoleLogger>
+    main argv
